ad83f44f4e04f59d05be5c613365b0fa6d9be446
Caio Cesar <caiocesarsts@gmail.com>
2023-11-24 11:14:46 -0300

    commit initial

---
+++ src/js/index.js
@@ -0,0 +1,83 @@
+const keyApi = "add43da3b59d43f487d15539232411";
+
+const buttonSearch = document.querySelector(".button");
+
+buttonSearch.addEventListener("click", async () => {
+    const city = document.getElementById("input-search").value;
+
+    if (city == "" || city == undefined) {
+        alert("Digite o nome de uma cidade");
+    }
+
+    const data = await searchCityData(city);
+
+    if (data) showDataOnScreen(data, city);
+});
+
+async function searchCityData(city) {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${keyApi}&q=${city}&aqi=yes&lang=pt`;
+
+    const response = await fetch(apiUrl);
+
+    if (response.status !== 200) return;
+
+    const data = await response.json();
+
+    return data;
+}
+
+function showDataOnScreen(data, city) {
+    const temperature = data.current.temp_c;
+    const condition = data.current.condition.text;
+    const humidity = data.current.humidity;
+    const windSpeed = data.current.wind_kph;
+    const uv = data.current.uv;
+    const temporalIcon = data.current.condition.icon;
+    const day = data.current.is_day;
+
+
+    changeImage(day);
+
+    document.getElementById("city").textContent = city;
+
+    document.getElementById("temperature").textContent = ` ${temperature} °C`;
+
+    document.getElementById("temporal-icon").setAttribute("src", temporalIcon);
+
+    document.getElementById("condition").textContent = condition;
+
+    document.getElementById("humidity").textContent = `${humidity}%`;
+
+    document.getElementById("wind-speed").textContent = `${windSpeed}km/h`;
+
+    showUvLevel(uv);
+
+    document.getElementById("uv").textContent = uv;
+}
+
+function changeImage(day) {
+    const image1 = "/src/images/imagens/sol2.jpg";
+    const image2 = "/src/images/imagens/auroraBoreal.jpg"
+
+    if (day == 1) {
+        let container = document.querySelector(".container");
+        container.style.backgroundImage = `url(${image1})`;
+    } else {
+        let container = document.querySelector(".container");
+        container.style.backgroundImage = `url(${image2})`;
+    }
+}
+
+function showUvLevel(uv) {
+    if (uv >= 0 && uv < 3) {
+        document.getElementById("uv-level").textContent = "(Baixo)";
+    } else if (uv >= 3 && uv < 6) {
+        document.getElementById("uv-level").textContent = "(Moderado)";
+    } else if (uv >= 6 && uv < 8) {
+        document.getElementById("uv-level").textContent = "(Alto)";
+    } else if (uv >= 8 && uv < 11) {
+        document.getElementById("uv-level").textContent = "(Muito Alto)";
+    } else {
+        document.getElementById("uv-level").textContent = "(Extremo)";
+    }
+}
